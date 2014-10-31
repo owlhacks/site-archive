1cf126c8c5f3ecde6f971a430766b0d8b4784c95
Sandile Keswa <me@sandile.io>
2014-10-31 04:08:19 -0400

    house cleaning; added transitions

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 // Main Module
 (function() {
     var SECTION_HREF_REGEX = /\#\/(\w+)/;
-    var $body = $('html, body');
+    var $body = $('html, body'),
+        images = [
+            '/img/bg.jpg',
+            '/img/owl.png',
+            '/img/about-build.png',
+            '/img/about-collaborate.png',
+            '/img/about-learn.png',
+            '/img/paleylibrary.jpg',
+            '/img/conwell.jpg',
+            '/img/icon-tudev.png',
+        ];
 
     function slideTo(scrollTop) {
         $body.animate({
@@ -19,10 +29,32 @@
         }
     }
 
+    function waitForImages() {
+        var i,
+            img,
+            loadedImages = 0,
+            callback = function() {
+                if (++loadedImages >= images.length) {
+                    $body.removeClass('loading');
+                    $('section#splash info').addClass('animated zoomIn');
+                }
+            };
+
+        for (i = 0; i < images.length; i++) {
+            img = new Image();
+            img.onload = callback;
+            img.src = images[i];
+        }
+    }
+
     $(function() {
         if ('onhashchange' in window) {
             $(window).bind('hashchange', doHashNavigation);
         }
-        setTimeout(doHashNavigation, 500);
+        waitForImages();
+
+        if (!Modernizr.flexbox) {
+            $('section#about .cutoff').addClass('shimmed');
+        }
     });
 })();
