44ac98f9b31cbc77b5c9db87f50734e5ce7fd14e
Sandile Keswa <me@sandile.io>
2014-10-31 02:58:22 -0400

    added autoscroll logic

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,28 @@
 // Main Module
 (function() {
+    var SECTION_HREF_REGEX = /\#\/(\w+)/;
+    var $body = $('html, body');
+
+    function slideTo(scrollTop) {
+        $body.animate({
+            scrollTop: scrollTop,
+        }, 1000, 'easeInOutExpo');
+    }
+
+    function doHashNavigation() {
+        var match, section, hash = window.location.hash;
+        if (match = hash.match(SECTION_HREF_REGEX)) {
+            section = $('section#' + match[1]).get(0);
+            if (section) {
+                slideTo($(section).offset().top);
+            }
+        }
+    }
+
     $(function() {
         if ('onhashchange' in window) {
-            $(window).bind('hashchange', function(e) {
-                console.log('hashchange', e);
-            });
+            $(window).bind('hashchange', doHashNavigation);
         }
+        setTimeout(doHashNavigation, 500);
     });
 })();
