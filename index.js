33527fec3df8a4559128ce3745e12f489dbcf141
Sandile Keswa <me@sandile.io>
2014-11-01 00:21:33 -0400

    added favicon

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express'),
-    path = require('path');
+    path = require('path'),
+    favicon = require('serve-favicon');
 
 var subscribe = require('./routes/subscribe');
 
 var app = express();
 
 app.set('port', process.env.PORT || 80);
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.status(200).sendFile(path.join(__dirname, 'public', 'dist', 'pages', 'index.html'));
 });
-
-app.post('/subscribe', subscribe.send_to_mailchimp);
 
 app.get('/*', function(req, res) {
     res.status(200).sendFile(path.join(__dirname, 'public', 'dist', 'pages', '404.html'));
