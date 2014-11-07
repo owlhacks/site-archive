5d58e7c2ef41992c37a252de4735c6df917503d0
Sandile Keswa <me@sandile.io>
2014-11-07 01:51:55 -0500

    dropped load time

--- public/js/main.js
+++ public/js/main.js
@@ -53,14 +53,14 @@
             img.src = images[i];
         }
 
-        // Institute a 8 second max wait time
+        // Institute a 3 second max wait time
         setTimeout(function() {
             loadingTimedOut = true;
             $progress.css('width', '200px');
             setTimeout(function() {
                 $body.removeClass('loading');
             }, 500);
-        }, 8000);
+        }, 3000);
     }
 
     $(function() {
